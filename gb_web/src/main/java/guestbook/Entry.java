852a2f661d0b86efd79a2eb3ed6a9908dd43c2b3
pakunz <pakunz@students.mail.uni-mannheim.de>
2013-09-14 11:00:58 +0200

    Implemented JPA persistence including - named query and generated id value (selection) - persistence.xml and update to web.xml - test methods with in-memory derby data source - comments and documentation

--- gb_web/src/main/java/guestbook/Entry.java
+++ gb_web/src/main/java/guestbook/Entry.java
@@ -2,12 +2,34 @@ package guestbook;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Basic;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "T_ENTRY")
+@NamedQuery(name = "getAllEntries", query = "select e from Entry e")
 public class Entry {
 
+	@Id
+	@GeneratedValue
+	private Long id; //Note that the id attributes of the entries are filled when they are added to the entity manager
+	@Basic
 	private String name;
+	@Basic
 	private String text;
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date date;
 
+	public Long getId() {
+		return id;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -30,6 +52,20 @@ public class Entry {
 
 	public void setDate(Date date) {
 		this.date = date;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Entry)) {
+			return false;
+		}
+		Entry entry = (Entry) obj;
+		//Note id generation remark (only be entity manager)
+		if(this.getId() != null && entry.getId() != null) {
+			return this.getId() == entry.getId();
+		} else {
+			return this.getName().equals(entry.getName()) && this.getText().equals(entry.getText()) && this.getDate().equals(entry.getDate());
+		}
 	}
 
 }
